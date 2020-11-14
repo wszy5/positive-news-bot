11f5673023a7e319f7dce066dc1fe3d8e85e488e
wszy5 <wikoszyman@wp.pl>
2020-11-15 00:03:16 +0100

    implemented inline mode to bot

--- bot.py
+++ bot.py
@@ -2,21 +2,29 @@ from scraper import main_news
 import telebot
 import random
 import os
+from telebot import types
 
-good_api_key = os.getenv("good_api_key")
+api_key = os.getenv("api_key")
+bot = telebot.TeleBot(api_key,parse_mode=None)
 
-bot = telebot.TeleBot(good_api_key,parse_mode=None)
+def make_default_keyboard():
+    """Makes default KeyBoard"""
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    itembtn1 = types.InlineKeyboardButton(text='/daj', )
+    itembtn2 = types.InlineKeyboardButton(text='/autor', )
+    markup.add(itembtn1, itembtn2)
+    return markup
 
 @bot.message_handler(commands=['daj'])
 def send_random_news(message):
     arr = main_news()
     random_news = random.choice(arr)
     arr.remove(random_news)
-    bot.send_message(message.chat.id,random_news)
+    bot.send_message(message.chat.id,random_news,reply_markup=make_default_keyboard())
 
 @bot.message_handler(commands=['autor'])
 def send_about(message):
-    bot.send_message(message.chat.id,'Bota stworzył Wiktor Szymański')
+    bot.send_message(message.chat.id,'Bota stworzył Wiktor Szymański',reply_markup=make_default_keyboard())
 
 
 bot.polling()
