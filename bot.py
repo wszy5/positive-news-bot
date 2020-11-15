0c54957e683b8e41cc8342ce48f0ea7c197ccf32
wszy5 <wikoszyman@wp.pl>
2020-11-15 16:08:50 +0100

    API Key was stored in separate file

--- bot.py
+++ bot.py
@@ -1,34 +1,34 @@
-import scraper
+from scraper import main_news,video_news
 import telebot
 import random
 import os
 from telebot import types
+from env import config
 
-api_key = os.getenv("api_key")
-bot = telebot.TeleBot(api_key,parse_mode=None)
+bot = telebot.TeleBot(config.api_key,parse_mode=None)
 
 def make_default_keyboard():
     """Makes default KeyBoard"""
     markup = types.ReplyKeyboardMarkup(row_width=2)
-    itembtn1 = types.InlineKeyboardButton(text='/losuj news', )
-    itembtn2 = types.InlineKeyboardButton(text='/video news', )
-    itembtn3 = types.InlineKeyboardButton(text='/info', )
+    itembtn1 = types.InlineKeyboardButton(text='/News', )
+    itembtn2 = types.InlineKeyboardButton(text='/VideoNews', )
+    itembtn3 = types.InlineKeyboardButton(text='/Info', )
     markup.add(itembtn1, itembtn2,itembtn3)
     return markup
 
-@bot.message_handler(commands=['losuj news'])
+@bot.message_handler(commands=['News'])
 def send_random_news(message):
     arr = main_news()
     random_news = random.choice(arr)
     bot.send_message(message.chat.id,random_news,reply_markup=make_default_keyboard())
 
-@bot.message_handler(commands=['video news'])
+@bot.message_handler(commands=['VideoNews'])
 def send_video_news(message):
-    arr = main_news()
-    random_news = random.choice(arr)
-    bot.send_message(message.chat.id,random_news,reply_markup=make_default_keyboard())
+    arr = video_news()
+    news = random.choice(arr)
+    bot.send_message(message.chat.id,news,reply_markup=make_default_keyboard())
 
-@bot.message_handler(commands=['info'])
+@bot.message_handler(commands=['Info'])
 def send_about(message):
     bot.send_message(message.chat.id,'Bota stworzył Wiktor Szymański',reply_markup=make_default_keyboard())
 
--- scraper.py
+++ scraper.py
@@ -23,4 +23,3 @@ def video_news():
     for i in body:
         arr.append(i.find("a").get("href"))
     return arr   
-
